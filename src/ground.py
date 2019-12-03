63bf4fe6bc494ba7680a6296c8c05867a0522bf0
Danaes <alvarez17dani@gmail.com>
2019-12-03 17:34:21 +0100

    Integration test

--- src/ground.py
+++ src/ground.py
@@ -1,15 +1,43 @@
+import spidev
+import mraa
+
+relayPin = 22
+resistor = 2200
+
 class Ground:
     
     temperature = 0
-    humidity = 0
+    weight = 0
+    relay = 0
+    spi = 0
 
-    def getTemperature(self):
-        self.temperature += 1
+    def __init__(self):
+        self.spi = spidev.SpiDev()
+        self.spi.open(1,0)
+
+        self.relay = mraa.Gpio(relayPin)
+        self.relay.dir(mraa.DIR_OUT)
+
+    def getWeight(self):
+        const = 35.0
+        adcValue = self.getDataFromSpi(0)
+        self.weight = (int(adcValue) - 102) / const
+        return self.weight
+    
+    def water(self, openValve):
+	    if openValve:
+		    self.relay.write(1)
+	    else:
+		    self.relay.write(0)
+    
+    def getTemperature(self): 
+        adcValue = self.getDataFromSpi(1)
+        self.temperature = (resistor*((1023/adcValue) - 1) - 1000)/3.875
+
         return self.temperature
 
-    def getHumidity(self):
-        self.humidity += 1
-        return self.humidity
-
-    def activeSolenoid(self):
-        print ("Activamos la electrovalvula")
+    
+    def getDataFromSpi(self, channel):
+        r = self.spi.xfer([1,(8+channel)<<4,0])
+        adcOut = ((r[1]&3) << 8) + r[2]
+        return adcOut
--- src/system.py
+++ src/system.py
@@ -5,26 +5,28 @@ import ground as g
 
 sensor_data = {
     'humidityExt'   : 0,
-    'humidityInt'   : 0,
     'temperatureExt': 0,
     'temperatureInt': 0,
-    'light'         : 0
+    'light'         : 0,
+    'weight'        : 0
 }
 
 async def manageData(env, gnd):
     while True:
         env.loadDataFromInternalSensors()
-        sensor_data['temperatureExt'] = env.getTemperature()
-        sensor_data['temperatureInt']  = gnd.getTemperature()
         sensor_data['light'] = env.getBrightness()
-        sensor_data['humidityExt'] = env.getHumidity()
-        sensor_data['humidityInt'] = gnd.getHumidity()
+        sensor_data['weight'] = gnd.getWeight()
 
         ## Actuadores
-        if (sensor_data['light'] > 200 ):
+        if (sensor_data['light'] < 200 ):
             env.turnOnLamp(1,1,1)
         else:
             env.turnOffLamp()
+
+        if(sensor_data['weight'] < 3):
+            gnd.water(True)
+        else:
+            gnd.water(False)
 
         await asyncio.sleep(1)
 
@@ -33,9 +35,9 @@ async def manageTB(pvd, env, gnd):
     while True:
         sensor_data['temperatureExt'] = env.getTemperature()
         sensor_data['temperatureInt']  = gnd.getTemperature()
+        sensor_data['weight'] = gnd.getWeight()
         sensor_data['light'] = env.getBrightness()
         sensor_data['humidityExt'] = env.getHumidity()
-        sensor_data['humidityInt'] = gnd.getHumidity()
         pvd.sendData(sensor_data)
         #print(f'Envío datos a TB: {sensor_data}') 
         await asyncio.sleep(2)
